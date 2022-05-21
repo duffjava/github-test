9a1ec1a1f31aca9bd6dd43b0b1ad9126b5097f4b
duff <905080721@qq.com>
2022-05-21 23:33:25 +0800

    第一次在hostfile中修改

--- src/main/java/com/atguigu/HelloGitTest.java
+++ src/main/java/com/atguigu/HelloGitTest.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class HelloGitTest {
     public static void main(String[] args) {
         System.out.println("Hello,world");
+        System.out.println("hello,hostfile");
     }
 }
