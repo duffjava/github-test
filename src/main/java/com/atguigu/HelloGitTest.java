a2aca79241fee8005e90bb95e256eff00ccb0901
duff <905080721@qq.com>
2022-05-21 23:36:48 +0800

    在master中测试冲突

--- src/main/java/com/atguigu/HelloGitTest.java
+++ src/main/java/com/atguigu/HelloGitTest.java
@@ -4,5 +4,6 @@ public class HelloGitTest {
     public static void main(String[] args) {
         System.out.println("Hello,world");
         System.out.println("hello,hostfile");
+        System.out.println("hello,master");
     }
 }
